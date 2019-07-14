b459eec51f3589bab150b5d62c8e6c8132dc08c0
hkicin1 <hkicin1@etf.unsa.ba>
2019-07-14 17:25:36 +0200

    Kreirana klasa Person

---
+++ src/sample/java/Person.java
@@ -0,0 +1,12 @@
+package sample.java;
+
+public class Person {
+    private int id;
+    private String name;
+    private String surname;
+    private String address;
+    private String postalNumber;
+    private String city;
+    private String userName;
+    private String password;
+}
