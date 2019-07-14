50157f774cb24b8fac71af1671f5ee4f90379bea
hkicin1 <hkicin1@etf.unsa.ba>
2019-07-14 12:42:54 +0200

    Prepravljen login screen.

--- src/sample/controllers/LoginController.java
+++ src/sample/controllers/LoginController.java
@@ -1,10 +1,27 @@
 package sample.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
+
+    public TextArea txtUsername;
+    public PasswordField pwdPassword   ;
+
+    private String username;
+    private String password;
+
     public void loginAction(ActionEvent actionEvent) {
+        username = txtUsername.getText();
+        password = pwdPassword.getText();
+    }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 }
