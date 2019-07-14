db50854ab278891e3276a059d7ee8b29d7f0f550
hkicin1 <hkicin1@etf.unsa.ba>
2019-07-14 17:40:06 +0200

    Kreirana klasa WorkHours

---
+++ src/sample/java/WorkHours.java
@@ -0,0 +1,52 @@
+package sample.java;
+
+import java.util.Date;
+
+public class WorkHours {
+    private int id;
+    private Employee employeeId;
+    private Date date;
+    private int workHours;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Employee getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Employee employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getWorkHours() {
+        return workHours;
+    }
+
+    public void setWorkHours(int workHours) {
+        this.workHours = workHours;
+    }
+
+    public WorkHours() {
+    }
+
+    public WorkHours(int id, Employee employeeId, Date date, int workHours) {
+        this.id = id;
+        this.employeeId = employeeId;
+        this.date = date;
+        this.workHours = workHours;
+    }
+}
