0eb89a2a2bc75247096ff846e6960605abdd3689
hkicin1 <hkicin1@etf.unsa.ba>
2019-07-14 17:54:04 +0200

    Kreirana klasa ProjectWorkHours

---
+++ src/sample/java/Position.java
@@ -0,0 +1,6 @@
+package sample.java;
+
+public class Position {
+    private int id;
+
+}
---
+++ src/sample/java/ProjectWorkHours.java
@@ -0,0 +1,24 @@
+package sample.java;
+
+import java.util.Date;
+
+public class ProjectWorkHours extends WorkHours {
+    private Project projectId;
+
+    public ProjectWorkHours(Project projectId) {
+        this.projectId = projectId;
+    }
+
+    public ProjectWorkHours(int id, Employee employeeId, Date date, int workHours, Project projectId) {
+        super(id, employeeId, date, workHours);
+        this.projectId = projectId;
+    }
+
+    public Project getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Project projectId) {
+        this.projectId = projectId;
+    }
+}
