53f6ef743f55e8401b876f12eee47e03270a0546
hkicin1 <hkicin1@etf.unsa.ba>
2019-07-14 17:56:23 +0200

    Kreirana klasa Position

--- src/sample/java/Position.java
+++ src/sample/java/Position.java
@@ -1,6 +1,50 @@
 package sample.java;
 
+import java.util.Date;
+
 public class Position {
     private int id;
+    private String name;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBeginningDate() {
+        return beginningDate;
+    }
+
+    public void setBeginningDate(Date beginningDate) {
+        this.beginningDate = beginningDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Position(int id, String name, Date beginningDate, Date endDate) {
+        this.id = id;
+        this.name = name;
+        this.beginningDate = beginningDate;
+        this.endDate = endDate;
+    }
+
+    private Date beginningDate;
+    private Date endDate;
 }
