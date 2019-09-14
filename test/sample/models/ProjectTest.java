f5e261d22d7c0354b1ca3e976561c97c58c55dbd
hkicin1 <hkicin1@etf.unsa.ba>
2019-09-14 20:09:57 +0200

    Kreiran test za klasu Project

--- test/sample/models/ProjectTest.java
+++ test/sample/models/ProjectTest.java
@@ -5,20 +5,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProjectTest {
+    Project project = new Project(1, "MojProjekat", 1);
 
     @Test
     void getName() {
+        assertEquals("MojProjekat", project.getName());
     }
 
     @Test
     void setName() {
+        project.setName("RS Projekat");
+        assertEquals("RS Projekat", project.getName());
     }
 
     @Test
     void getActivity() {
+        assertEquals(1, project.getActivity());
     }
 
     @Test
     void setActivity() {
+        project.setActivity(0);
+        assertEquals(0, project.getActivity());
     }
 }
