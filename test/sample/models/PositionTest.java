e50bdded00b29d8ff7ed15a48bf93870cfd226cd
hkicin1 <hkicin1@etf.unsa.ba>
2019-09-14 20:13:51 +0200

    Kreiran test za klasu Position

--- test/sample/models/PositionTest.java
+++ test/sample/models/PositionTest.java
@@ -5,12 +5,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PositionTest {
+    Position position = new Position(2, "programmer");
+
+    @Test
+    void getId() {
+        assertEquals(2, position.getId());
+    }
+
+    @Test
+    void setId() {
+        position.setId(1);
+        assertEquals(1, position.getId());
+    }
 
     @Test
     void getName() {
+        assertEquals("programmer", position.getName());
     }
 
     @Test
     void setName() {
+        position.setName("tester");
+        assertEquals("tester", position.getName());
     }
 }
